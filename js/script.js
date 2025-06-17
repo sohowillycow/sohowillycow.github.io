65a7bac87c4dfe96525cafd9a903f94e1c867c77
sohowillycow <sohowillycow@gmail.com>
2025-06-17 20:17:02 +0800

    chore: drop jquery (#4)

--- js/script.js
+++ js/script.js
@@ -1,138 +1,162 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+(() => {
+  /* ------------------------------------------------------------------
+     Search form toggle
+     ------------------------------------------------------------------ */
+  const searchWrap = document.getElementById('search-form-wrap');
+  let isSearchAnim = false;
+  const searchAnimDuration = 200;
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
+  const startSearchAnim = () => { isSearchAnim = true; };
+  const stopSearchAnim = cb => {
+    setTimeout(() => {
       isSearchAnim = false;
-      callback && callback();
+      if (cb) cb();
     }, searchAnimDuration);
   };
 
-  $('.nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
+  document.querySelectorAll('.nav-search-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      if (isSearchAnim) return;
+      startSearchAnim();
+      searchWrap.classList.add('on');
+      stopSearchAnim(() => {
+        const input = document.querySelector('.search-form-input');
+        if (input) input.focus();
+      });
     });
   });
 
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
+  const searchInput = document.querySelector('.search-form-input');
+  if (searchInput) {
+    searchInput.addEventListener('blur', () => {
+      startSearchAnim();
+      searchWrap.classList.remove('on');
+      stopSearchAnim();
+    });
+  }
+
+  /* ------------------------------------------------------------------
+     Share box handling
+     ------------------------------------------------------------------ */
+  document.body.addEventListener('click', () => {
+    document.querySelectorAll('.article-share-box.on').forEach(box => box.classList.remove('on'));
   });
 
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
+  document.body.addEventListener('click', e => {
+    const link = e.target.closest('.article-share-link');
+    if (!link) return;
     e.stopPropagation();
 
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      title = $this.attr('data-title'),
-      offset = $this.offset();
+    const url = link.dataset.url;
+    const encodedUrl = encodeURIComponent(url);
+    const id = 'article-share-box-' + link.dataset.id;
+    const title = link.dataset.title;
+    let box = document.getElementById(id);
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
+    if (box) {
+      if (box.classList.contains('on')) {
+        box.classList.remove('on');
         return;
       }
     } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"><span class="fa fa-twitter"></span></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"><span class="fa fa-facebook"></span></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"><span class="fa fa-pinterest"></span></a>',
-            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"><span class="fa fa-linkedin"></span></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
+      const html = `
+        <div id="${id}" class="article-share-box">
+          <input class="article-share-input" value="${url}">
+          <div class="article-share-links">
+            <a href="https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${encodedUrl}" class="article-share-twitter" target="_blank" title="Twitter"><span class="fa fa-twitter"></span></a>
+            <a href="https://www.facebook.com/sharer.php?u=${encodedUrl}" class="article-share-facebook" target="_blank" title="Facebook"><span class="fa fa-facebook"></span></a>
+            <a href="http://pinterest.com/pin/create/button/?url=${encodedUrl}" class="article-share-pinterest" target="_blank" title="Pinterest"><span class="fa fa-pinterest"></span></a>
+            <a href="https://www.linkedin.com/shareArticle?mini=true&url=${encodedUrl}" class="article-share-linkedin" target="_blank" title="LinkedIn"><span class="fa fa-linkedin"></span></a>
+          </div>
+        </div>`;
+      box = document.createRange().createContextualFragment(html).firstElementChild;
+      document.body.appendChild(box);
     }
 
-    $('.article-share-box.on').hide();
+    document.querySelectorAll('.article-share-box.on').forEach(el => {
+      if (el !== box) el.classList.remove('on');
+    });
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
+    const rect = link.getBoundingClientRect();
+    box.style.top = rect.top + window.pageYOffset + 25 + 'px';
+    box.style.left = rect.left + window.pageXOffset + 'px';
+    box.classList.add('on');
+  });
+
+  document.body.addEventListener('click', e => {
+    if (e.target.closest('.article-share-box')) e.stopPropagation();
+  });
+
+  document.body.addEventListener('click', e => {
+    if (e.target.classList.contains('article-share-box-input')) {
+      e.target.select();
+    }
+  });
+
+  document.body.addEventListener('click', e => {
+    const link = e.target.closest('.article-share-box-link');
+    if (!link) return;
     e.preventDefault();
     e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+    window.open(link.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
   });
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
+  /* ------------------------------------------------------------------
+     Image captions and lightbox links
+     ------------------------------------------------------------------ */
+  document.querySelectorAll('.article-entry').forEach((entry, i) => {
+    entry.querySelectorAll('img').forEach(img => {
+      const parent = img.parentElement;
+      if (parent.classList.contains('fancybox') || parent.tagName.toLowerCase() === 'a') return;
 
-      var alt = this.alt;
+      const alt = img.alt;
+      if (alt) {
+        const span = document.createElement('span');
+        span.className = 'caption';
+        span.textContent = alt;
+        img.insertAdjacentElement('afterend', span);
+      }
 
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
+      const wrapper = document.createElement('a');
+      wrapper.href = img.src;
+      wrapper.setAttribute('data-fancybox', 'gallery');
+      wrapper.setAttribute('data-caption', alt);
+      parent.insertBefore(wrapper, img);
+      wrapper.appendChild(img);
     });
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
+    entry.querySelectorAll('.fancybox').forEach(node => {
+      node.setAttribute('rel', 'article' + i);
     });
   });
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
+  /* ------------------------------------------------------------------
+     Mobile navigation toggle
+     ------------------------------------------------------------------ */
+  const container = document.getElementById('container');
+  let isMobileNavAnim = false;
+  const mobileNavAnimDuration = 200;
 
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
+  const startMobileNavAnim = () => { isMobileNavAnim = true; };
+  const stopMobileNavAnim = () => {
+    setTimeout(() => { isMobileNavAnim = false; }, mobileNavAnimDuration);
   };
 
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
+  const mainNavToggle = document.getElementById('main-nav-toggle');
+  if (mainNavToggle) {
+    mainNavToggle.addEventListener('click', () => {
+      if (isMobileNavAnim) return;
+      startMobileNavAnim();
+      container.classList.toggle('mobile-nav-on');
+      stopMobileNavAnim();
+    });
   }
 
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
-})(jQuery);
+  const wrap = document.getElementById('wrap');
+  if (wrap) {
+    wrap.addEventListener('click', () => {
+      if (isMobileNavAnim || !container.classList.contains('mobile-nav-on')) return;
+      container.classList.remove('mobile-nav-on');
+    });
+  }
+})();
